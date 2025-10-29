a2b20100609b2aa0c0d5989c474e03ccd2250120
Ckane55 <161179424+Ckane55@users.noreply.github.com>
2025-10-28 16:19:35 -0800

    Playing around with Password Hashes. Added a login functionality that compares your hashed password in the DHT with the one you entered

--- SuperNode.py
+++ SuperNode.py
@@ -3,7 +3,7 @@ import socket
 import asyncio
 from kademlia.network import Server
 import json
-
+import bcrypt
 async def run():
     node = Server()
     hostname = socket.gethostname()
@@ -11,12 +11,29 @@ async def run():
     print(ip)
     await node.listen(5678)
 
+    await node.bootstrap([("10.5.0.2", 5678)])
 
+    try:
+        with open("data.json") as f:
+            data = json.load(f)
+
+    except FileNotFoundError:
+       print("ERROR")
     
-    # set a value for the key "my-key" on the network
-    #await node.set("node1", ip)
+    await node.set("users", json.dumps(data["users"]))
+
+
+
+        
+
+
+
     while True:
        await asyncio.sleep(3600)
+
    
+                
+        
+
 
 asyncio.run(run())
--- node2.py
+++ node2.py
@@ -1,11 +1,12 @@
 import asyncio
 from kademlia.network import Server
 import socket
-import time
-
+import bcrypt
+import json
+node = Server()
 async def run():
     # Create a node and start listening on port 5678
-    node = Server()
+    
     await node.listen(5679)
     hostname = socket.gethostname()
     ip = socket.gethostbyname(hostname)
@@ -14,14 +15,35 @@ async def run():
     # replace 123.123.123.123 with the IP of another node and optionally
     # give as many ip/port combos as you can for other nodes.
     await node.bootstrap([("10.5.0.2", 5678)])
+    await login()
 
-    # set a value for the key "my-key" on the network
-    await node.set("node1", ip)
+    
 
-    await asyncio.sleep(5)
+    
 
-    # get the value associated with "my-key" from the network
-    result = await node.get("node1")
-    print(result)
+
+async def login():
+    
+    answer = input("Returning User? Y/N ")
+
+    if answer == "Y":
+        usrname = input("Enter Username:")
+        psswrd = input("Enter Password:")
+        users = await node.get("users")
+        users = json.loads(users)
+
+        login_info = users.get(usrname)
+        if bcrypt.checkpw(psswrd.encode(), login_info.get("password").encode()):
+            print("WELCOME")
+        
+        else:
+            print("WRONG PASSWORD")
+
+
+
+
+
+
+
 
 asyncio.run(run())
